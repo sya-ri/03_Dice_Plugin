bcd08e0e3a79fc9326503302bf6dbd0417c35d01
sya-ri <sya79lua@gmail.com>
2021-03-18 19:54:30 +0900

    サイコロの面の数を指定できるようになった

--- src/main/kotlin/com/github/syari/plugin/dice/CommandCreator.kt
+++ src/main/kotlin/com/github/syari/plugin/dice/CommandCreator.kt
@@ -9,10 +9,11 @@ object CommandCreator {
             aliases = listOf("d")
             execute {
                 val amount = args.getOrNull(0)?.toIntOrNull()
+                val max = args.getOrNull(1)?.toIntOrNull() ?: 6
                 if (amount != null) {
                     var sum = 0
                     repeat(amount) {
-                        sum += (1..6).random()
+                        sum += (1..max).random()
                     }
                     sender.send("&fサイコロを振った結果は &6$sum &fです")
                 } else {
